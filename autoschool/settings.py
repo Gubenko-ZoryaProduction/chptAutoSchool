4bfc010faf2ad74aa1ed0f4c631e1865700a6115
Oleksandr Gubenko <sasha0420011@gmail.com>
2020-02-13 14:12:00 +0200

    Add allowed host heroku server

--- autoschool/settings.py
+++ autoschool/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "chpt-autoschool.herokuapp.com",
+    "chpt-autoschool.com",
+]
 
 # Application definition
 
