4c893f1dabdcee8337f6ddf605574c703d4cbf1e
Oleksandr Gubenko <sasha0420011@gmail.com>
2020-02-13 23:49:26 +0200

    Some update. Try fix route

--- chptautoschool/views.py
+++ chptautoschool/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+
+
 # from django.views.generic.base import View
 
 
 # Create your views here.
 def index(request):
-    return render(request, "pages/index.html")
+    return render(request, "index.html")
+
+
+def courses(request):
+    return render(request, "courses.html")
+
+
+def contacts(request):
+    return render(request, "contacts.html")
+
+
+def reviews(request):
+    return render(request, "reviews.html")
+
+
+def training(request):
+    return render(request, "training.html")
+
+
+def useful(request):
+    return render(request, "useful.html")
