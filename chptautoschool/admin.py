31b968d98d7d7ee297018155c0c6bcc5b72ed557
Oleksandr Gubenko <sasha0420011@gmail.com>
2020-02-14 10:32:35 +0200

    Register model in admin

--- chptautoschool/admin.py
+++ chptautoschool/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from chptautoschool import models
+from .models import *
 # Register your models here.
+admin.site.register(Respond)
+admin.site.register(Useful)
+admin.site.register(DriversCourses)
+admin.site.register(Contacts)
+admin.site.register(BaseEducation)
+admin.site.register(ClassRoom)
+admin.site.register(Teachers)
 
